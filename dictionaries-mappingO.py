12c0b3e521c4315603ffd92f6acc55e753b33781
Hachima <tam9a5.2018.kt@gmail.com>
2024-10-20 00:19:52 +0700

    update20/10

---
+++ dictionaries-mappingO.py
@@ -0,0 +1,16 @@
+#create dictionaries as D
+D = {'food': 'milfTea', 'quality': 4, 'color': 'pink'}
+print(D)
+#print index compoment in dictionaries
+print(D['quality'])
+#add 1 to 'quality' value
+D['quality'] += 1
+print(D['quality'])
+#init D
+D = {}
+#create keys bt assignment
+D['name'] = 'tonthientam'
+D['job'] = 'dev'
+D['age'] = 21
+print(D)
+print(D['name'])
